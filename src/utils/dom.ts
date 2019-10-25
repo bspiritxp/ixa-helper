a1ea716c04ef088d7421978251cb439081768d65
Quanlin Chen <quanlin.chen@nwea.org>
2019-10-25 01:29:10 -0700

    Add tslint script, new rules

--- src/utils/dom.ts
+++ src/utils/dom.ts
@@ -1,64 +1,64 @@
-import { Village } from '@/items';
-import _ from 'lodash';
-import Optional from './tool';
+import { Village } from '@/items'
+import _ from 'lodash'
+import Optional from './tool'
 
 export const query = (s: string, doc: Document|HTMLElement = document) =>
-    Optional.ofNullable(doc.querySelector(s) as HTMLElement|null);
-export const queryAll = (s: string, doc: Document|HTMLElement = document) => doc.querySelectorAll(s);
+    Optional.ofNullable(doc.querySelector(s) as HTMLElement|null)
+export const queryAll = (s: string, doc: Document|HTMLElement = document) => doc.querySelectorAll(s)
 
 // 从dom中查询位置信息
 export const queryLocGroup = (selector: string) => {
-    const items: Village[] = [];
-    queryAll(selector).forEach(el => items.push(new Village(Optional.of(el as HTMLElement))));
-    return new Set(items);
-};
+    const items: Village[] = []
+    queryAll(selector).forEach(el => items.push(new Village(Optional.of(el as HTMLElement))))
+    return new Set(items)
+}
 
 export const create = (tagName: string, idName: string, isShow: boolean = false) => {
-    const result = document.createElement(tagName);
-    result.id = idName;
-    result.style.visibility = isShow ? 'visible' : 'hidden';
-    return result;
-};
+    const result = document.createElement(tagName)
+    result.id = idName
+    result.style.visibility = isShow ? 'visible' : 'hidden'
+    return result
+}
 
 const appendBody = (el: HTMLElement) => {
-    document.body.append(el);
-    return el;
-};
+    document.body.append(el)
+    return el
+}
 
 const prependBody = (el: HTMLElement) => {
-    document.body.prepend(el);
-    return el;
-};
+    document.body.prepend(el)
+    return el
+}
 
-export const createAdd = _.flow([create, appendBody]);
+export const createAdd = _.flow([create, appendBody])
 
-export const createAddTop = _.flow([create, prependBody]);
+export const createAddTop = _.flow([create, prependBody])
 
 export const createUnique = (tagName: string, idName: string, isShow: boolean = false): HTMLElement => {
-    return query(`${tagName}#${idName}`).getOrElse(() => createAdd(tagName, idName, isShow));
-};
+    return query(`${tagName}#${idName}`).getOrElse(() => createAdd(tagName, idName, isShow))
+}
 
 export const parseDom = (domStr: string) => {
-    const contanier = document.createElement('div');
-    contanier.innerHTML = domStr;
-    return contanier;
-};
+    const contanier = document.createElement('div')
+    contanier.innerHTML = domStr
+    return contanier
+}
 
 export const setCss = (el: HTMLElement, css: {[key: string]: string}) => {
-    let cssText = '';
+    let cssText = ''
     _.forOwn(css, (value, key) => {
-        cssText += `${key}: ${value};`;
-    });
-    el.style.cssText = cssText;
-};
+        cssText += `${key}: ${value};`
+    })
+    el.style.cssText = cssText
+}
 
-interface Pos {left: number; top: number; }
+interface Pos {left: number; top: number }
 
 export const getAbsolutePos = (el: HTMLElement, initValue: Pos = {left: 0, top: 0}): Pos => {
-    const thisPos = {left: initValue.left + el.offsetLeft, top: initValue.top + el.offsetTop};
-    const parent = el.offsetParent;
+    const thisPos = {left: initValue.left + el.offsetLeft, top: initValue.top + el.offsetTop}
+    const parent = el.offsetParent
     if (parent && parent.tagName !== 'BODY') {
-        return getAbsolutePos(parent as HTMLElement, thisPos);
+        return getAbsolutePos(parent as HTMLElement, thisPos)
     }
-    return thisPos;
-};
+    return thisPos
+}
--- src/utils/io.ts
+++ src/utils/io.ts
@@ -3,32 +3,32 @@ const get = (url: string, data: BodyInit): Promise<Response> => new Promise((res
     return fetch(url, {
         method: 'GET',
         body: data,
-    });
-});
+    })
+})
 
 const post = (url: string, data: BodyInit): Promise<Response> => new Promise<Response>((resolve: CallableFunction) => {
     return fetch(url, {
         method: 'POST',
         body: data,
-    });
-});
+    })
+})
 
 const updateDom = (method: VoidFunction): Promise<void> => new Promise((resolve: CallableFunction) => {
     try {
-        method();
+        method()
     } catch (err) {
         // console.error(err);
     } finally {
-        resolve();
+        resolve()
     }
-});
+})
 
 const changeName = (vid: number, newName: string): Promise<boolean> => new Promise((resolve: CallableFunction) => {
-    return;
-});
+    return
+})
 
 export default {
     post,
     get,
     updateDom,
-};
+}
