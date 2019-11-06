1120cb245d1917a4c25230e101e0b272f6057467
Jochen.He <hjc.xp.cn@gmail.com>
2019-11-06 16:20:06 +0800

    modify get function in io

--- src/utils/io.ts
+++ src/utils/io.ts
@@ -1,25 +1,16 @@
-export const get = (url: string, data?: BodyInit): Promise<Document> =>
-    new Promise((resolve: CallableFunction, reject: CallableFunction) => {
-    return fetch(url, {
+import {compose} from 'ramda'
+
+// **NOTE**  don't write .catch of fetch block in this file. please use try...catch block in async function
+
+export const get = (url: string, data?: BodyInit): Promise<Response> => fetch(url, {
         method: 'GET',
         body: data,
-    }).then(response => response.text())
-        .then(html => {
-            const parser = new DOMParser()
-            const doc = parser.parseFromString(html, 'text/html')
-            return resolve(doc)
-        }).catch(error => {
-            return reject('Failed to fetch page', error)
-        })
-})
+    })
 
-export const post = (url: string, data: BodyInit): Promise<Response> =>
-    new Promise<Response>((resolve: CallableFunction) => {
-    return fetch(url, {
+export const post = (url: string, data: BodyInit): Promise<Response> => fetch(url, {
         method: 'POST',
         body: data,
     })
-})
 
 export const updateDom = (method: VoidFunction): Promise<void> => new Promise((resolve: CallableFunction) => {
     try {
@@ -31,6 +22,7 @@ export const updateDom = (method: VoidFunction): Promise<void> => new Promise((r
     }
 })
 
-const changeName = (vid: number, newName: string): Promise<boolean> => new Promise((resolve: CallableFunction) => {
-    return
-})
+const htmlParser = (txt: string) => (new DOMParser()).parseFromString(txt, 'text/html')
+
+export const getText = compose((pr: Promise<Response>) => pr.then(resp => resp.text()), get)
+export const getHtml = compose((pt: Promise<string>) => pt.then(htmlParser), getText)
--- test/util_test.ts
+++ test/util_test.ts
@@ -1,21 +1,28 @@
-import Optional from '../src/utils/tool'
+// tslint:disable: no-console
 import { defaultTo } from 'ramda'
+import { get } from '../src/utils/io'
+import Optional from '../src/utils/tool'
+
 enum REPORT_TYPE {
     DISCOVERY = '秘境探索',
     SPACE_ATTACK = '空き地攻撃',
 }
 
-
 function testOpional() {
-    let testValue: string | null;
+    let testValue: string | null
     testValue = 'ok'
     const r1 = defaultTo('hello')(testValue)
-    console.log(r1);
+    console.log(r1)
 }
 
 function testSplitText() {
-    const testStr1 = '戦利品「  木材210  綿210  鉄210  糧210 」を獲得しました';
-    const testStr2 = '戦利品「  鉄2160 」を獲得しました';
+    const testStr1 = '戦利品「  木材210  綿210  鉄210  糧210 」を獲得しました'
+    const testStr2 = '戦利品「  鉄2160 」を獲得しました'
 }
 
-testOpional()
+async function testGet() {
+    const r = await get('https://www.baidu.com')
+    console.log(r)
+}
+
+testGet()
