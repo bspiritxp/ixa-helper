aa59fa95f222943a892a1fd340775150400b88c1
Quanlin Chen <quanlin.chen@nwea.org>
2020-02-27 19:17:21 -0800

    introduce feature to update project version, renable build button after timeout

--- ixa-helper.user.js
+++ ixa-helper.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name     ixa-helper
-// @version  1.1.0
+// @version  1.2.0
 // @match http*://*.sengokuixa.jp/*
 // @match http*://*sengokuixa.jp*
 // @grant    none
--- scripts/build.js
+++ scripts/build.js
@@ -1,14 +1,29 @@
 const path = require('path');
 const fs = require('fs');
+const readline = require('readline');
 const rootPath = path.resolve(__dirname, '..');
 const workPath = path.resolve(rootPath, 'dist');
 const distFileName = 'ixa-helper.user.js';
+const VERSION_REGEX = /\d+.\d+.\d+/m
+const stage = process.env.STAGE;
 
 // fs.exists(path.resolve(workPath, distFileName), r => {
 //     if (r) return;
 const src = path.resolve(rootPath, distFileName);
 const target = path.resolve(workPath, distFileName);
 try {
+  //bump up user script version
+  fs.readFile(src, 'utf8', (err, data) => {
+    if(err){
+      return console.log(err)
+    }
+    const result = data.replace(VERSION_REGEX, bumpVersion(data.match(VERSION_REGEX)))
+    fs.writeFile(src, result, 'utf8', (err) => {
+      if(err) {
+        return console.log(err)
+      }
+    })
+  })
     fs.copyFileSync(src, target);
 } catch (Exception) {
     // nothing to do 
@@ -23,5 +38,21 @@ fs.readFile(path.resolve(workPath, 'main.js'), (err, data) => {
     })
 })
 // })
+
+//version naming convention, major.minor.patch
+//major is for breaking change, no plan for this for now
+const bumpVersion = (matchedString) => {
+  if(matchedString) {
+    const version = matchedString[0].split('.').map(num => parseInt(num, 10));
+    console.log(stage)
+    if(stage === 'release') {
+      //update minor version
+      version[1]++
+    } else {
+      version[2]++
+    }
+    return version.join('.')
+  }
+}
 
 console.info('plugin file genarate done.');
